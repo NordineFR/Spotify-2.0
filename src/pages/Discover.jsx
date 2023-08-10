854afdf08567bc78ddc2269f4f75b10c6803280f
NordineFR <farisseaz@outlook.fr>
2023-08-10 18:27:37 +0100

    added song card and music player

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,10 +1,14 @@
+import {useDispatch,useSelector} from 'react-redux';
+
 import {Error,Loader,SongCard} from '../components';
 import { genres } from '../assets/constants';
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 const Discover = () => {
+    const dispatch = useDispatch();
+    const {activeSong,isPlaying} = useSelector((state)=>state.player);
     const {data,isFetching,error} = useGetTopChartsQuery();
     const genreTitle = 'Pop';
-    console.log(data);
+    
     if(isFetching) return <Loader title="Loading songs..."/>;
     if(error) return <Error />;
     return (
@@ -23,7 +27,7 @@ const Discover = () => {
             
             <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
                 {(data.tracks)?.map((song,i)=>(
-                    <SongCard key={song.key} song={song} i={i} />
+                    <SongCard key={song.key} song={song} i={i} isPlaying={isPlaying} activeSong={activeSong} data={data} />
                 ))}
             </div>
         </div>
