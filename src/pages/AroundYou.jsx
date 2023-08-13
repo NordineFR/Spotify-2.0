2edb9fae354b935cdfa14622df801da6515399de
NordineFR <farisseaz@outlook.fr>
2023-08-13 22:07:35 +0100

    aroud you page setup

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,5 +1,21 @@
-import React from 'react';
+import { useState,useEffect } from "react";
+import { Axios } from "axios";
+import { useSelector } from "react-redux";
 
-const CountryTracks = () => <div>CountryTracks</div>;
 
-export default CountryTracks;
+import { Error,Loader,Songcard } from "../components";
+const AroundYou = () =>{
+    const [country, setCountry] = useState('');
+    const [loading, setLoading] = useState(true);
+    const {activeSong,isPlaying} = useSelector((state)=>state.player);
+
+    useEffect(() => {
+      
+    }, [country]);
+    
+    return (
+        <div ></div>
+    )
+}
+
+export default AroundYou;
