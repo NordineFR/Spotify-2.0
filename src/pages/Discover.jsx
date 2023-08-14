ef0acdb6ee26a95811e1bc6a576914bde1552249
NordineFR <farisseaz@outlook.fr>
2023-08-14 21:55:34 +0100

    added genre filter to discover page

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,22 +1,38 @@
 import {useDispatch,useSelector} from 'react-redux';
+import { useState,useEffect } from "react";
 
 import {Error,Loader,SongCard} from '../components';
 import { genres } from '../assets/constants';
-import { useGetTopChartsQuery } from '../redux/services/shazamCore';
+import { useGetChartListQuery,useGetSongsByGenreQuery } from '../redux/services/shazamCore';
+import { selectGenreListId } from '../redux/features/playerSlice';
 const Discover = () => {
     const dispatch = useDispatch();
-    const {activeSong,isPlaying} = useSelector((state)=>state.player);
-    const {data,isFetching,error} = useGetTopChartsQuery();
-    const genreTitle = 'Pop';
-    
+    const {activeSong,isPlaying,genreListId} = useSelector((state)=>state.player);
+    const genreTitle = genres.find(({value})=>value === genreListId)?.title;
+    // extra api code 
+    const [listId, setListId] = useState({});
+    const {data:ListData,isFetching:isFetchingListData,errorlist} = useGetChartListQuery();
+    const {data,isFetching,error} = useGetSongsByGenreQuery(listId.listid || 'genre-global-chart-1');
+
+    useEffect(() => {
+        if (ListData && ListData.global.genres) {
+          (ListData.global.genres).forEach((genredata) => {
+            if (genredata.urlPath === genreListId.toLowerCase()) {
+                setListId(genredata);
+            }
+          });
+        }
+      }, [ListData,genreListId]);
+    //  end 
+
     if(isFetching) return <Loader title="Loading songs..."/>;
     if(error) return <Error />;
     return (
         <div className="flex flex-col">
             <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
                 <h2 className="font-bold text-3xl text-white text-left">Discover {genreTitle}</h2>
-                <select name="" id="" onChange={()=>{}}
-                value=""
+                <select name="" id="" onChange={(e)=>{dispatch(selectGenreListId(e.target.value))}}
+                value={genreListId || 'pop'}
                 className='bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5'
                 >
                     {
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -6,12 +6,13 @@ export const shazamCoreApi = createApi({
     baseQuery:fetchBaseQuery({
         baseUrl:'https://shazam.p.rapidapi.com',
         prepareHeaders:(headers)=>{
-            headers.set('X-RapidAPI-Key','620089e2d5mshe04a1be2fed7031p128975jsn2c384a7b8125');
+            headers.set('X-RapidAPI-Key','bf53143c5amsh021d3dc3a89c74cp1f7558jsn5356486077fe');
             return headers;
         },
     }),
     endpoints : (builder)=>({
         getTopCharts : builder.query({query:() => '/charts/track'}),
+        getSongsByGenre : builder.query({query:(listid) => `/charts/track?listId=${listid}`}),
         getSongDetails:builder.query({query:(songid)=>`/songs/get-details?key=${songid}`}),
         getSongRelated:builder.query({query:(songid)=>`/songs/list-recommendations?key=${songid}`}),
         getArtistDetails:builder.query({query:(artistId)=>`/artists/get-details?id=${artistId}`}),
@@ -29,4 +30,5 @@ export const {
     useGetArtistTopSongsQuery,
     useGetChartListQuery,
     useGetSongsByCountryQuery,
+    useGetSongsByGenreQuery,
 }= shazamCoreApi;
